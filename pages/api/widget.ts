24b7db42aaba0c1990b1479c94f0fc91b5ed12fa
Kevin Reed <kevvvinreed@gmail.com>
2023-12-27 15:07:39 -0800

    update build files

---
+++ pages/api/widget.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const directoryPath = path.resolve("./widgets/dist/assets");
+  const filePattern = /^app-.*\.js$/;
+  try {
+    const files = fs.readdirSync(directoryPath);
+
+    // Search for the file
+    const foundFile = files.find((file) => filePattern.test(file));
+
+    if (foundFile) {
+      const filePath = path.join(directoryPath, foundFile);
+      const fileContents = fs.readFileSync(filePath, "utf8");
+
+      res.setHeader("Content-Type", "application/javascript");
+      res.send(fileContents);
+    } else {
+      res.status(400).send("No widget app build file found");
+    }
+  } catch (error) {
+    res.status(400).send("Error reading the directory");
+  }
+}
