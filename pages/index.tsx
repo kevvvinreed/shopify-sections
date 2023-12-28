e3f7f8234ef0aa525327789b60001878bd29c1c9
Kevin Reed <kevvvinreed@gmail.com>
2023-12-27 17:16:45 -0800

    update model code to include a color shifter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
 import { NextPage } from "next";
 import { IPageProps } from "./_app";
+import { useLayoutEffect } from "react";
 
 const Home: NextPage<IPageProps> = ({}) => {
+  useLayoutEffect(() => {
+    // Create a new script element
+    const script = document.createElement("script");
+    script.src = "https://shopify-sections-yeqggr54rq-uk.a.run.app/api/widget";
+    script.id = "widget-script";
+
+    // Attach the load event listener to the script
+    script.addEventListener("load", function () {
+      if ((window as any).initReactComponent) {
+        (window as any).initReactComponent({
+          section_id: "three-js",
+          shop_id: "root",
+        });
+        console.log("initReactComponent called");
+      } else {
+        console.error("initReactComponent function not defined.");
+      }
+    });
+
+    // Append the script to the document
+    document.body.appendChild(script);
+
+    // Optional: Clean up the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <>
-      <div></div>
+      <div id="root" style={{ height: "100%", width: "100%" }}></div>
     </>
   );
 };
--- widgets/src/components/ThreeJS/index.tsx
+++ widgets/src/components/ThreeJS/index.tsx
@@ -8,6 +8,7 @@ import {
   PresentationControls,
 } from "@react-three/drei";
 import React from "react";
+import styles from "./index.module.css";
 
 const AnyCanvas = Canvas as any;
 const AnySuspense = Suspense as any;
@@ -20,7 +21,8 @@ const Model = (props: any): any => {
   const { scene, nodes, materials } = useGLTF(
     `https://pub-201533c97c3b4e169c75945e8e2f95fc.r2.dev/lambo.glb`
   );
-  useLayoutEffect(() => {
+  const { selectedColor } = props;
+  useEffect(() => {
     scene.traverse(
       (obj: any) =>
         obj.type === "Mesh" && (obj.receiveShadow = obj.castShadow = true)
@@ -33,16 +35,19 @@ const Model = (props: any): any => {
     Object.assign(materials.WhiteCar, {
       roughness: 0.0,
       metalness: 0.1,
-      emissive: new Color("#500000"),
+      emissive: new Color(selectedColor),
       envMapIntensity: 0.5,
     });
-  }, [scene, nodes, materials]);
+  }, [scene, nodes, materials, selectedColor]);
   return <primitive object={scene} {...props} />;
 };
 
 const ThreeJS: React.FC<Props> = () => {
   const [rotation, setRotation] = useState(0.9999);
+  const [selectedOption, setSelectedOption] = useState<number>(0);
   const modelRef = useRef<Object3D>(null);
+
+  const colors = ["#ddd", "#f80", "#1b1"];
 
   useEffect(() => {
     const handleScroll = (event: WheelEvent) => {
@@ -58,29 +63,50 @@ const ThreeJS: React.FC<Props> = () => {
   }, []);
   return (
     <>
-      <AnyCanvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
-        <color attach="background" args={["#101010"]} />
-        <fog attach="fog" args={["#101010", 10, 20]} />
-        <AnySuspense fallback={null}>
-          <Environment
-            path={`https://pub-201533c97c3b4e169c75945e8e2f95fc.r2.dev/cube`}
-          />
-          <PresentationControls
-            speed={1.5}
-            global
-            zoom={0.7}
-            polar={[-0.1, Math.PI / 4]}
-          >
-            <Stage environment={null} intensity={0.5} shadows={false}>
-              <Model
-                ref={modelRef}
-                scale={0.01}
-                rotation={[0.2, rotation, 0]}
+      <div className={styles.container}>
+        <div className={styles.sceneContainer}>
+          <AnyCanvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+            <color attach="background" args={["#101010"]} />
+            <fog attach="fog" args={["#101010", 10, 20]} />
+            <AnySuspense fallback={null}>
+              <Environment
+                path={`https://pub-201533c97c3b4e169c75945e8e2f95fc.r2.dev/cube`}
               />
-            </Stage>
-          </PresentationControls>
-        </AnySuspense>
-      </AnyCanvas>
+              <PresentationControls
+                speed={1.5}
+                global
+                zoom={0.7}
+                polar={[-0.1, Math.PI / 4]}
+              >
+                <Stage environment={null} intensity={0.5} shadows={false}>
+                  <Model
+                    ref={modelRef}
+                    scale={0.01}
+                    rotation={[0, rotation, 0]}
+                    selectedColor={colors[selectedOption]}
+                  />
+                </Stage>
+              </PresentationControls>
+            </AnySuspense>
+          </AnyCanvas>
+        </div>
+        <div className={styles.editorContainer}>
+          <div className={styles.colorSelectorLabel}>Color</div>
+          <div className={styles.colorSelectorRow}>
+            {colors.map((color: string, index: number) => {
+              return (
+                <div
+                  className={styles.selectorOption}
+                  style={{ backgroundColor: color }}
+                  onClick={() => {
+                    setSelectedOption(index);
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
