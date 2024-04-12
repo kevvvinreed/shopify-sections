c5819a1f6898ca3ca0ea53fc50caa69714713f23
Kevin Reed <kevvvinreed@gmail.com>
2024-04-12 12:32:47 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-const env = {};
 
 module.exports = {
-  nextConfig,
-  env,
+  reactStrictMode: true,
+  env: {},
 };
